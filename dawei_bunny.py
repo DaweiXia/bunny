30968f1e38a8c8911b6aa2e2e7747f702e519526
Dawei Xia <xiadw_public@163.com>
2016-11-22 21:37:01 +0800

    draw a clock that can record how long you have playing the game

--- dawei_bunny.py
+++ dawei_bunny.py
@@ -96,6 +96,15 @@ while 1:
     for badguypos in badguyspos:
         screen.blit(badguy, badguypos)
 
+    # 6.7 - Draw clock
+    font = pygame.font.Font(None, 24)
+    survivedtext = font.render(str(pygame.time.get_ticks()/60000)+":"+
+                               str(pygame.time.get_ticks()/1000%60).zfill(2),
+                               True, (0,0,0))
+    textRect = survivedtext.get_rect()
+    textRect.topright = [635, 5]
+    screen.blit(survivedtext, textRect)
+
     # 7 - Update the screen
     pygame.display.flip()
 
